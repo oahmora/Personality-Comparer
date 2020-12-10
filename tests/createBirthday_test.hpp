297bccfa1e20d985a805650efb53d09d1bf06e66
Yonas-A <yadam002@ucr.edu>
2020-12-10 02:19:13 -0800

    Adding the unit test for CreateBirthday class.

---
+++ tests/createBirthday_test.hpp
@@ -0,0 +1,39 @@
+#ifndef __CREATEBIRTHDAY_TESTS_HPP__
+#define __CREATEBIRTHDAY_TESTS_HPP__
+
+
+#include "gtest/gtest.h"
+#include <iostream>
+
+#include "../src/createBirthday.cpp"
+
+
+TEST(CreateBirthdayTest, CreateBirthdayTest1) {
+    CreateBirthday *_validate = new CreateBirthday();
+    _validate->ask_birthday();
+
+    std::string str ("Date = [");
+    str += std::to_string(_validate->get_birthday()->get_month());
+    str += "/" + std::to_string(_validate->get_birthday()->get_day());
+    str += "/" + std::to_string(_validate->get_birthday()->get_year());
+    str += "]";
+ 
+    EXPECT_EQ(_validate->get_birthday()->to_string(),str);
+} 
+
+TEST(CreateBirthdayTest, CreateBirthdayTest2) {
+    CreateBirthday *_validate = new CreateBirthday();
+    _validate->ask_birthday();
+
+    testing::internal::CaptureStdout();
+    std::cout << *_validate->get_birthday();
+    std::string output = testing::internal::GetCapturedStdout();
+    
+    std::string str ("");
+    str += std::to_string(_validate->get_birthday()->get_month());
+    str += "/" + std::to_string(_validate->get_birthday()->get_day());
+    str += "/" + std::to_string(_validate->get_birthday()->get_year());
+    EXPECT_EQ(output,str);
+} 
+
+#endif // __CREATEBIRTHDAY_TESTS_HPP__
