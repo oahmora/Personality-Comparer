bfe75e6d0ed9bbe7ee0cf9f330a9c520058c396c
Yonas-A <yadam002@ucr.edu>
2020-12-10 23:31:59 -0800

    Fixes #2

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 #include "../tests/date_test.hpp"
+#include "../tests/createBirthday_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
